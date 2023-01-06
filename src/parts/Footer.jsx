5434cf48899695e4f2bbb5d6c00d4eff332e1949
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-06 12:43:30 +0700

    create Footer.jsx

---
+++ src/parts/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="flex justify-center py-4 px-8 text-center">
+      <p class="text-sm">
+        &copy; Copyright • All Rights Reserved LuxSpace | Fajar Fadillah A
+      </p>
+    </footer>
+  );
+}
