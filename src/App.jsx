229fd92a8aa47821e5e380df6534d0c9ab5359be
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-06 18:46:52 +0700

    rename the HomePage file to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/" element={<Home />} />
         </Routes>
       </Router>
     </>
