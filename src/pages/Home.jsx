4b0a80c38359ba3a43943d13a501fcd7c1d4aebb
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-07 13:02:38 +0700

    rename folder Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 import Header from "../parts/Header";
-import Hero from "../parts/HomePage/Hero";
-import BrowseRoom from "../parts/HomePage/BrowseRoom";
-import JustArrived from "../parts/HomePage/JustArrived";
+import Hero from "../parts/Homepage/Hero";
+import BrowseRoom from "../parts/Homepage/BrowseRoom";
+import JustArrived from "../parts/Homepage/JustArrived";
 import Clients from "../parts/Clients";
 import Sitemap from "../parts/Sitemap";
 import Footer from "../parts/Footer";
